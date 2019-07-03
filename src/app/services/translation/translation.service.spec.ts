f041038f0382968f14f18a82eee59c9d27a4f343
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-07-03 17:30:38 -0500

    Translation service test ready

--- src/app/services/translation/translation.service.spec.ts
+++ src/app/services/translation/translation.service.spec.ts
@@ -3,10 +3,48 @@ import { TestBed } from '@angular/core/testing';
 import { TranslationService } from './translation.service';
 
 describe('TranslationService', () => {
+  let service: TranslationService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: TranslationService = TestBed.get(TranslationService);
+  it('should create TranslationService', () => {
+    service = TestBed.get(TranslationService);
     expect(service).toBeTruthy();
+  });
+
+  it('should changeLanguageText', () => {
+    service = TestBed.get(TranslationService);
+    const newLanguage = 1;
+    service.changeLanguageText(newLanguage);
+    expect(service.currentLanguage).toEqual(
+      service.getLanguages()[newLanguage]
+    );
+    expect(service.currentText.value.CLOSE).toEqual('Cerrar');
+  });
+
+  it('should getCorrectTranslation', () => {
+    const translations = [
+      {
+        language: {
+          name: 'es'
+        },
+        value: 'Hola'
+      },
+      {
+        language: {
+          name: 'en'
+        },
+        value: 'Hello'
+      }
+    ];
+    service = TestBed.get(TranslationService);
+    expect(service.getCorrectTranslation(translations).value).toEqual('Hello');
+  });
+
+  it('should getLanguages', () => {
+    service = TestBed.get(TranslationService);
+    const languages = service.getLanguages();
+    expect(languages.length).toEqual(3);
+    expect(languages[0].code).toEqual('en');
   });
 });
